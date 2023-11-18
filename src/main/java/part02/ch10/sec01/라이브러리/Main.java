aa7d58309cf9d33c3701e9bd288e2abf049761c6
nooblette <min9hyuk@gmail.com>
2023-11-18 12:37:45 +0900

    feat : ch10 - 라이브러리와 모듈(10.1. 라이브러리)

---
+++ src/main/java/part02/ch10/sec01/라이브러리/Main.java
@@ -0,0 +1,14 @@
+package part02.ch10.sec01.라이브러리;
+
+// 외부 라이브러리 import
+import package1.A;
+import package2.B;
+public class Main {
+	public static void main(String[] args) {
+		A a = new A();
+		a.method();
+
+		B b = new B();
+		b.method();
+	}
+}
